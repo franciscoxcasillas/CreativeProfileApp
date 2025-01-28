f1c8f3aaecfc2f030c4ca81bfd38f70479c4e0c7
Francisco Casillas <chiccasillas@gmail.com>
2025-01-28 12:55:59 -0600

    Add icon, name, hobbies and description using auxiliary views and imported fonts

--- CreativeProfileApp/ContentView.swift
+++ CreativeProfileApp/ContentView.swift
@@ -10,12 +10,60 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NameAndProfession()
+            ProfileImage()
+                .padding()
+            Description()
         }
         .padding()
+    }
+}
+
+struct NameAndProfession: View {
+    var body: some View {
+        VStack {
+            Text("Francisco Javier Casillas")
+                .font(.custom("AmericanTypewriter", size: 23))
+            Text("iOS Developer")
+                .font(.custom("BebasNeue", size: 20))
+        }
+    }
+}
+
+struct ProfileImage: View {
+    var body: some View {
+        Image("ProfilePicture")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 80)
+            .clipShape(Circle())
+    }
+}
+
+struct Description: View {
+    var body: some View {
+        VStack {
+            Text("Hobbies")
+                .bold()
+            HStack {
+                Image(systemName: "pencil")
+                    .foregroundColor(.blue)
+                Image(systemName: "books.vertical")
+                    .foregroundColor(.pink)
+                Image(systemName: "figure.skateboarding")
+                    .foregroundColor(.green)
+                Image(systemName: "music.microphone")
+                    .foregroundColor(.orange)
+                Image(systemName: "pianokeys")
+                    .foregroundColor(.purple)
+            }
+            .padding(7)
+            Text("Brief")
+                .bold()
+                .padding(7)
+            Text("I’m a passionate iOS developer and a computer systems engineering student. I love mobile app development, music, graphic design, photography, and reading. My interests combine creativity and technology, driving me to build innovative and visually appealing projects.")
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
