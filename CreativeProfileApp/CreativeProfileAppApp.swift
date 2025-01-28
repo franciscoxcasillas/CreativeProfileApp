9f7ecce1a9e513b01ad7281e7b0aa5a45a4fc556
Francisco Casillas <chiccasillas@gmail.com>
2025-01-28 11:43:56 -0600

    Initial Commit

---
+++ CreativeProfileApp/CreativeProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativeProfileAppApp.swift
+//  CreativeProfileApp
+//
+//  Created by Francisco Jean on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CreativeProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
